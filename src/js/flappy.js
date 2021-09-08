96d6d74c19da758ce545a7fb55ea2f6475ec291a
Moreira <jvitormoreirad@hotmail.com>
2021-09-08 12:24:02 -0300

    test

--- src/js/flappy.js
+++ src/js/flappy.js
@@ -80,7 +80,7 @@ function Bird(alturaDoJogo){
     let voando = false
 
     this.elemento = novoElemento('img', 'bird')
-    this.elemento.src =  '../src/assets/images/Passaro.png' 
+    this.elemento.src =  'https://github.com/JVMoreiraD/flappy-bird/blob/master/src/assets/images/passaro.png' 
 
     this.getY = () => parseInt(this.elemento.style.bottom.split('px')[0])
     this.setY = y => this.elemento.style.bottom = `${y}px`
